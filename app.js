1445279f62f7c7b33dab41df897711a9d0df3614
Zsombor Franczia <me@frzsombor.com>
2019-04-30 02:26:59 +0200

    Autodetection, file analyzer, file selection

--- app.js
+++ app.js
@@ -6,64 +6,145 @@ const path = require('path');
 const inquirer = require('inquirer');
 const { exec } = require('child_process');
 
+const analyzer = require('./lib/analyzer')
+
+/* PREPARING */
+
 const platforms = ['win32', 'darwin'];
 const platform = process.platform;
-
-// products: [
-//     "DJI Assistant 2",
-//     "DJI Assistant 2 For Mavic",
-//     "DJI Assistant 2 For Phantom",
-// ],
-
-global.TARGET = {
-    dir: null,
-    type: null, // 'ALL' | 'Mavic' | 'Phantom'
-    version: null,
-};
 
 if (!platforms.includes(platform)) {
     console.error('This operating system ('+ platform +') is not supported!');
     return;
 }
 
-inquirer.registerPrompt('filefolder', require('inquirer-filefolder-prompt'));
+inquirer.registerPrompt(
+    'filefolder', require('inquirer-filefolder-prompt')
+);
 
-console.log('Welcome to %appname%, the latest DJI Assistant 2 unlocker!');
-
-// Scan local dir for 'DJI Assistant 2.exe'
-console.log(__dirname);
-fs.readdirSync('.').forEach(file => {
-    console.log(file);
-});
-
-// exec("dir", function (error, stdout, stderr) {
-//     console.log('stdout: ' + stdout);
-//     console.log('stderr: ' + stderr);
-//     if (error !== null) {
-//         console.log('exec error: ' + error);
-//     }
-// });
-
-/*
-inquirer.prompt({
-  type: 'filefolder',
-  name: 'file',
-  message: 'Please select the file.',
-  dialog: {
-      type: 'OpenFileDialog',
-      config: {
-          'title': 'Open',
-          //...
-      },
-  },
-  validate: function(file) {
-    if (file.length === 0) {
-      return 'No file selected.';
+const crossPlatformConfig = {
+    'win32': {
+        'target': {
+            'name': 'DJI Assistant 2.exe',
+            'defaultDir': 'C:\\Program Files (x86)\\DJI Product\\DJI Assistant 2\\',
+        }
+    },
+    'darwin': {
+        'target': {
+            'name': 'Assistant 2.app',
+            'defaultDir': '/Applications/',
+        }
     }
-    return true;
-  }
-}).then(answers => {
-  console.log(JSON.stringify(answers, null, '  '));
+};
+
+const CONFIG = crossPlatformConfig[platform];
+
+let TARGET = {
+    path: null,
+    type: null, // 'ALL' | 'Mavic' | 'Phantom'
+    version: null,
+};
+
+/* STAGE 0 */
+
+console.log('');
+console.log('Welcome to %appname%, the latest DJI Assistant 2 unlocker!');
+console.log('');
+
+// Scan current dir for 'DJI Assistant 2.exe'
+let detected = false;
+let currentDir = __dirname;
+// currentDir = 'd:\\Program Files (x86)\\DJI Assistant 2';
+// currentDir = 'd:\\Program Files (x86)\\DJI Assistant 2 For Mavic';
+// currentDir = 'd:\\Program Files (x86)\\DJI Assistant 2 For Phantom\\';
+fs.readdirSync(currentDir).forEach(file => {
+    if (file === CONFIG.target.name) {
+        detected = true;
+        console.log('"' + file + '" found in current folder! Analysing...');
+
+        let fileFullPath = currentDir + path.sep + file;
+        analyzer.analyzeOn[platform](fileFullPath, function(results, error){
+            if (error) {
+                console.log('The application in the current folder is not supported!');
+                console.log(error);
+                console.log('');
+                stage1();
+                return;
+            }
+            stage2(results);
+        });
+    }
 });
-*/
+
+if (!detected) {
+    stage1();
+}
+
+/* STAGE 1 */
+
+function stage1() {
+    console.log('Please select the DJI Assistant that you want to unlock!');
+
+    inquirer.prompt({
+        type: 'filefolder',
+        name: 'file',
+        message: 'Select "' + CONFIG.target.name + '".',
+        messageCTA: 'Press <enter> to select a file.',
+        dialog: {
+            type: 'OpenFileDialog',
+            config: {
+                win32: {
+                    title: 'Please select ""DJI Assistant 2.exe""',
+                }
+                //...
+            },
+        },
+        validate: function(file) {
+            if (file.length === 0) {
+                return 'No file selected.';
+            }
+            else if (path.basename(file) !== CONFIG.target.name) {
+                return 'Unknown file selected: ' + path.basename(file);
+            }
+            return true;
+        }
+    }).then(answers => {
+        console.log('');
+        console.log('Analysing...');
+        analyzer.analyzeOn[platform](answers.file, function(results, error){
+            if (error) {
+                console.log('The selected application is not supported!');
+                console.log(error);
+                console.log('');
+                stage1();
+                return;
+            }
+            stage2(results);
+        });
+    });
+}
+
+function stage2(results) {
+    TARGET.path = results.path;
+    TARGET.type = results.type;
+    TARGET.version = results.version || null;
+    // console.log(TARGET);
+
+    console.log(
+        'Detected: ' +
+        'DJI Assistant 2 (' + TARGET.type + ') ' +
+        'version ' + ((TARGET.version) ? 'v'+TARGET.version : '[N/A]') + '.'
+    );
+
+    console.log('Target directory: ' + TARGET.path);
+    console.log('');
+
+    //TODO: check supported versions
+    //TODO: check if already patched (+restore)
+
+    stage3();
+}
+
+function stage3() {
+}
 
